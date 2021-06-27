f8e843ca179dea5cece0505e42be11f1141b6ae7
icarosolon <icaro.solon.is@gmail.com>
2021-06-27 19:36:43 -0300

    Autenticação

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -4,14 +4,30 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Tweet;
+use Livewire\WithPagination; //paginando sem recarregar pagina
 
 class ShowTweets extends Component
 {
-    public $message = 'Apenas um teste';
+    use WithPagination; //paginando sem recarregar pagina
+
+    public $content = '';
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
-        $tweets = Tweet::with('user')->get();
+        $tweets = Tweet::with('user')->paginate(2);
         return view('livewire.show-tweets', compact('tweets'));
+    }
+
+    public function create()
+    {
+        $this->validate();
+        Tweet::create([
+            'content' => $this->content,
+            'user_id' => auth()->user()->id,
+        ]);
+        $this->content = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,15 +1,26 @@
 <div>
     Show Tweets
-    <p>{{ $message }}</p>
+    <p>{{ $content }}</p>
 
 
-    <input type="text"
-        name="message"
-        id="message"
-        wire:model="message">
+
 
         <hr>
+        <form action="" method="post" wire:submit.prevent="create">
+            <input type="text"
+            name="content"
+            id="content"
+            wire:model="content">
+            @error('content')
+                {{ $message }}
+            @enderror
+            <button type="submit">Criar Tweet</button>
+        </form>
         @foreach ($tweets as $tweet)
             <p>{{ $tweet->user->name }} - {{ $tweet->content }}</p>
         @endforeach
+        <hr>
+        <div>
+            {{ $tweets->links() }}
+        </div>
 </div>
